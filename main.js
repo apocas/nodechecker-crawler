8a30ff0a9a94e4252bf9e6c93bb8c9dc5b75db34
apocas <petermdias@gmail.com>
2014-01-14 04:32:50 +0000

    major revamp

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 var Npmt = require('./lib/npmt');
 
-var npmt = new Npmt('130.185.82.11', 7556);
+var redis_ip = process.env.REDIS_HOST || '127.0.0.1';
+var redis_port = process.env.REDIS_PORT || 6379;
+var balancer_ip = process.env.BALANCER_HOST || '127.0.0.1';
+var balancer_port = process.env.BALANCER_PORT || 5000;
+
+var npmt = new Npmt(redis_ip, redis_port, balancer_ip, balancer_port);
 npmt.run();
