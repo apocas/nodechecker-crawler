a94cc0bf308beaa37e23f0b6b18c880e751679c6
apocas <petermdias@gmail.com>
2014-01-19 01:21:00 +0000

    redis off mongodb on

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 var Npmt = require('./lib/npmt');
 
-var redis_ip = process.env.REDIS_HOST || '127.0.0.1';
-var redis_port = process.env.REDIS_PORT || 6379;
+var mongo_ip = process.env.MONGODB_HOST || '127.0.0.1';
+var mongo_port = process.env.MONGODB_PORT || 27017;
 var balancer_ip = process.env.BALANCER_HOST || '127.0.0.1';
 var balancer_port = process.env.BALANCER_PORT || 5000;
 
-var npmt = new Npmt(redis_ip, redis_port, balancer_ip, balancer_port);
+var npmt = new Npmt(mongo_ip, mongo_port, balancer_ip, balancer_port);
 npmt.run();
